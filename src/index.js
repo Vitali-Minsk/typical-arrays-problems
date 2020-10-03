a9e531f367d4c698d3ef55c9818ceca3978b2a11
burakou_vitali <vitali.burakou@gmail.com>
2020-10-03 18:37:51 +0300

    fix: test complited

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+    if (array !== undefined) {
+        if (array.length > 0) {
+            return Math.min.apply(null, array);
+        }
+    } return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+  if (array !== undefined) {
+        if (array.length > 0) {
+            return Math.max.apply(null, array);
+        }
+    } return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+  if (array !== undefined) {
+        if (array.length > 0) {
+            for (var i = 0, sum = 0; i < array.length; i++) {
+   sum = parseInt(array[i]) + sum;
+  }
+  return (sum / i);
+   }
+    } return 0;
 }
+
+
